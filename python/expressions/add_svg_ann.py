43ea869e962928cfe5ad357940255315e6ee2403
petrandreev <petrandreev@aol.com>
2018-06-09 01:11:43 +0200

    created utility to add SVG annotations to features

---
+++ python/expressions/add_svg_ann.py
@@ -0,0 +1,27 @@
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+from PyQt5.QtCore import QSizeF
+
+@qgsfunction(args='auto', group='Lanzen')
+def add_svg_ann(layerId, path, feature, parent):
+	QgsMessageLog.logMessage('layerId: ' + layerId + ', path: ' + path, 'Lanzen', level=Qgis.Info)
+	currentLayer = QgsProject.instance().mapLayer(layerId)
+	svgAnnotation = QgsSvgAnnotation(iface.mapCanvas())
+	svgAnnotation.setMapLayer(currentLayer)
+	symbol = QgsMarkerSymbol()
+	symbol.setSize(0)
+	svgAnnotation.setMarkerSymbol(symbol)
+	svgAnnotation.setHasFixedMapPosition(True)
+	svgAnnotation.setMapPosition(feature.geometry().asPoint())
+	svgAnnotation.setFrameSize(QSizeF(80, 64))
+	svgAnnotation.setFilePath(path)
+	QgsProject.instance().annotationManager().addAnnotation(svgAnnotation)
+	# If caching is enabled, a simple canvas refresh might not be sufficient
+	# to trigger a redraw and you must clear the cached image for the layer
+	if iface.mapCanvas().isCachingEnabled():
+		currentLayer.triggerRepaint()
+	else:
+		iface.mapCanvas().refresh()
+	return
+	
--- python/expressions/show_scan.py
+++ python/expressions/show_scan.py
@@ -1,13 +1,15 @@
 import os, base64
 from qgis.core import *
-from qgis.utils import iface
-from PyQt5.QtCore import QSizeF
+from qgis.utils import *
 
-@qgsfunction(args='auto', group='Custom')
+@qgsfunction(args='auto', group='Lanzen')
 def show_scan(url, mimeType, feature, parent):
-	svg = """<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><image xlink:href="data:image/{0};base64,{1}" height="256" width="320" /></g></svg>"""
+	QgsMessageLog.logMessage('show_scan: url: ' + url + ', mimeType: ' + mimeType, 'Lanzen', level=Qgis.Info)
+	svg = """<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><image xlink:href="data:image/{0};base64,{1}" height="64" width="80" /></g></svg>"""
 	imagePath = url[8:] ##the URL must be the local one with 'file:' protocol
+	QgsMessageLog.logMessage('show_scan: imagePath: ' + imagePath, 'Lanzen', level=Qgis.Info)
 	path = imagePath + '.svg'
+	QgsMessageLog.logMessage('show_scan: path: ' + path, 'Lanzen', level=Qgis.Info)
 	if os.path.exists(path):
 		return path
 	with open(imagePath, "rb") as imageFile:
@@ -15,15 +17,4 @@ def show_scan(url, mimeType, feature, parent):
 		newsvg = svg.format(mimeType.lower(), data).replace("b'", "'", 1).replace('\n','')
 	with open(path, 'w') as f:
 		f.write(newsvg)
-	#svgAnnotation = QgsSvgAnnotation(iface.mapCanvas())
-	#symbol = QgsMarkerSymbol()
-	#symbol.setSize(10)
-	#svgAnnotation.setMarkerSymbol(symbol)
-	#geom = feature.geometry()
-	#point = geom.asPoint()
-	#svgAnnotation.setMapPosition(point)
-	#svgAnnotation.setMapLayer(iface.activeLayer())
-	#svgAnnotation.setFrameSize(QSizeF(300, 200))
-	#svgAnnotation.setFilePath(path)
-	#canvas.refresh()
 	return path
